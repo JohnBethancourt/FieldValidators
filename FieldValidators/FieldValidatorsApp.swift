03d5a7864a939fe00e23ff0cd04af7be079dbde5
John B <jbethancourt.ctr@revacomm.com>
2021-01-05 17:31:25 -1000

    Initial Commit

---
+++ FieldValidators/FieldValidatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FieldValidatorsApp.swift
+//  FieldValidators
+//
+//  Created by John Bethancourt on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FieldValidatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
